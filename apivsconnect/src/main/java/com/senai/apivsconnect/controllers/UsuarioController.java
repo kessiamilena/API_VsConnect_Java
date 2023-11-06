ce33ae8601384ce469c6eeafd218945784df6ba9
Kessia-Milena <kessia.milena124@yahoo.com>
2023-11-06 20:34:14 -0300

    Inclusão repositório - Aula API VsConnect SENAI - JAVA

---
+++ apivsconnect/src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -0,0 +1,80 @@
+package com.senai.apivsconnect.controllers;
+
+import com.senai.apivsconnect.dtos.UsuarioDto;
+import com.senai.apivsconnect.models.UsuarioModel;
+import com.senai.apivsconnect.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/usuarios", produces = {"application/json"})
+public class UsuarioController {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> listarUsuarios() {
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<Object> exibirUsuario(@PathVariable(value = "idUsuario") UUID id) {
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
+
+        if (usuarioBuscado.isEmpty()) {
+            // Retornar usuário não encontrado
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioBuscado.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> cadastrarUsuario(@RequestBody @Valid UsuarioDto usuarioDto) {
+        if ( usuarioRepository.findByEmail(usuarioDto.email()) != null ) {
+            // Não pode cadastrar
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Esse email já está cadastrado");
+        }
+
+        UsuarioModel usuario = new UsuarioModel();
+        BeanUtils.copyProperties(usuarioDto, usuario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+    }
+
+    @PutMapping("/{idUsuario}")
+    public ResponseEntity<Object> editarUsuario(@PathVariable(value = "idUsuario") UUID id, @RequestBody @Valid UsuarioDto usuarioDto) {
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
+
+        if ( usuarioBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        UsuarioModel usuario = usuarioBuscado.get();
+        BeanUtils.copyProperties(usuarioDto, usuario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity<Object> deletarUsuario(@PathVariable(value = "idUsuario") UUID id) {
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
+
+        if ( usuarioBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        usuarioRepository.delete(usuarioBuscado.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Usuário deletado com sucesso!");
+    }
+
+}
